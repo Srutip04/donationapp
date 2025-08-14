d25ebaeeda0af81567d868ad137c4fad7fc9d51b
srutip04 <subuhishruti75@gmail.com>
2025-08-15 04:37:42 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import MainNaviagtion from './navigation/MainNavigation';
+const App = () => {
+  
+  return (
+    <NavigationContainer>
+     <MainNaviagtion/>
+    </NavigationContainer>
+  );
+};
+
+export default App;
---
+++ components/Title/style.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+import { getFontFamily } from "../../assets/fonts/helper";
+import { scaleFontSize } from "../../assets/styles/scaling";
+
+const style = StyleSheet.create({
+    title:{
+        color: '#022150',
+        fontSize: scaleFontSize(24),
+        fontFamily: getFontFamily('Inter', '600'),
+    },
+});
+
+export default style;
