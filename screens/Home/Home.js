b20c2cf44ce9d099103a93421a451474249b49a2
srutip04 <subuhishruti75@gmail.com>
2025-08-18 23:32:26 +0530

    Button Component

--- screens/Home/Home.js
+++ screens/Home/Home.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { SafeAreaView } from 'react-native';
 
 import Header from '../../components/Header/Header';
+import Button from '../../components/Button/Button';
 import style from './style';
 import globalStyle from '../../assets/styles/globalStyles';
 
@@ -9,6 +10,13 @@ const Home = ({ navigation }) => {
   return (
     <SafeAreaView style={[globalStyle.backgroundWhite, globalStyle.flex]}>
       <Header title={'Azzahri A.'} type={1} />
+      <Button
+        title={'Donate'}
+        onPress={() => {
+          console.log('You just pressed me !!');
+        }}
+      />
+      <Button title={'Donate'} isDisabled={true} />
     </SafeAreaView>
   );
 };
