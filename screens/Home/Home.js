cffaf4559c64bcbbdad7e34370b98e21f53acc7c
srutip04 <subuhishruti75@gmail.com>
2025-08-19 01:49:01 +0530

    styling single donation item

--- screens/Home/Home.js
+++ screens/Home/Home.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { SafeAreaView } from 'react-native';
+import { SafeAreaView,View } from 'react-native';
 import Header from '../../components/Header/Header';
 import Button from '../../components/Button/Button';
 import Tab from '../../components/Tab/Tab';
@@ -8,6 +8,7 @@ import Search from '../../components/Search/Search';
 import SingleDonationItem from '../../components/SingleDonationItem/SingleDonationItem';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { horizontalScale } from '../../assets/styles/scaling';
 
 import style from './style';
 import globalStyle from '../../assets/styles/globalStyles';
@@ -29,14 +30,29 @@ const Home = ({ navigation }) => {
       <FontAwesomeIcon icon={faSearch} />
       <Search onSearch={value =>{console.log(value)}}/> */}
 
-      <SingleDonationItem
-        uri={
-          'https://img.pixers.pics/pho_wat(s3:700/FO/44/24/64/31/700_FO44246431_ab024cd8251bff09ce9ae6ecd05ec4a8.jpg,525,700,cms:2018/10/5bd1b6b8d04b8_220x50-watermark.png,over,305,650,jpg)/stickers-cactus-cartoon-illustration.jpg.jpg'
-        }
-        badgeTitle={'Environment'}
-        donationTitle={'Tree Cactus'}
-        price={44}
-      />
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          paddingHorizontal: horizontalScale(24),
+        }}>
+        <SingleDonationItem
+          uri={
+            'https://img.pixers.pics/pho_wat(s3:700/FO/44/24/64/31/700_FO44246431_ab024cd8251bff09ce9ae6ecd05ec4a8.jpg,525,700,cms:2018/10/5bd1b6b8d04b8_220x50-watermark.png,over,305,650,jpg)/stickers-cactus-cartoon-illustration.jpg.jpg'
+          }
+          badgeTitle={'Environment'}
+          donationTitle={'Tree Cactus'}
+          price={44}
+        />
+        <SingleDonationItem
+          uri={
+            'https://img.pixers.pics/pho_wat(s3:700/FO/44/24/64/31/700_FO44246431_ab024cd8251bff09ce9ae6ecd05ec4a8.jpg,525,700,cms:2018/10/5bd1b6b8d04b8_220x50-watermark.png,over,305,650,jpg)/stickers-cactus-cartoon-illustration.jpg.jpg'
+          }
+          badgeTitle={'Environment'}
+          donationTitle={'Tree Cactus'}
+          price={44}
+        />
+      </View>
     </SafeAreaView>
   );
 };
