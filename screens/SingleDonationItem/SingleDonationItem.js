2ed447bf0818a5fc6dc5b3c11f220ff4c620f07f
srutip04 <subuhishruti75@gmail.com>
2025-09-06 01:22:06 +0530

    Route Params & Single Donation Item screen

--- screens/SingleDonationItem/SingleDonationItem.js
+++ screens/SingleDonationItem/SingleDonationItem.js
@@ -1,20 +1,47 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import {SafeAreaView, ScrollView} from 'react-native';
+import { Image, View, Text, SafeAreaView, ScrollView } from 'react-native';
+
 import BackButton from '../../components/BackButton/BackButton';
+import Badge from '../../components/Badge/Badge';
+import Header from '../../components/Header/Header';
+import Button from '../../components/Button/Button';
+
 import globalStyle from '../../assets/styles/globalStyles';
 import style from './style';
 
-const SingleDonationItem = ({navigation}) => {
+const SingleDonationItem = ({ navigation, route }) => {
   const donationItemInformation = useSelector(
     state => state.donations.selectedDonationInformation,
   );
+  const categoryInformation = route.params.categoryInformation;
+
   console.log(donationItemInformation);
   return (
     <SafeAreaView style={[globalStyle.backgroundWhite, globalStyle.flex]}>
       <ScrollView showsVerticalScrollIndicator={false} style={style.container}>
         <BackButton onPress={() => navigation.goBack()} />
+        <Image
+          source={{ uri: donationItemInformation.image }}
+          style={style.image}
+        />
+        <View style={style.badge}>
+          <Badge title={categoryInformation.name} />
+        </View>
+        <Header type={1} title={donationItemInformation.name} />
+        <Text style={style.description}>
+          {donationItemInformation.description}
+          {donationItemInformation.description}
+          {donationItemInformation.description}
+          {donationItemInformation.description}
+          {donationItemInformation.description}
+          {donationItemInformation.description}
+          {donationItemInformation.description}
+        </Text>
       </ScrollView>
+      <View style={style.button}>
+        <Button title={'Donate'} />
+      </View>
     </SafeAreaView>
   );
 };
