0e0fb9744fda9accca85661df22edbb47eb6cda2
srutip04 <subuhishruti75@gmail.com>
2025-09-09 14:06:25 +0530

    localhost server side code integration

--- screens/Payment/Payment.js
+++ screens/Payment/Payment.js
@@ -1,17 +1,61 @@
-import React, {useState} from "react";
-import {SafeAreaView, ScrollView, View, Text, Alert} from 'react-native';
-import globalStyle from "../../assets/styles/globalStyles";
-import Header from "../../components/Header/Header";
-import style from "./style";
-import { useSelector } from "react-redux";
-import {CardForm, StripeProvider} from '@stripe/stripe-react-native';
-import { STRIPE_PUBLISHABLE_KEY } from "../../constants/App";
-import Button from "../../components/Button/Button";
+import React, { useState } from 'react';
+import { SafeAreaView, ScrollView, View, Text, Alert } from 'react-native';
+import globalStyle from '../../assets/styles/globalStyles';
+import Header from '../../components/Header/Header';
+import style from './style';
+import { useSelector } from 'react-redux';
+import {
+  CardForm,
+  StripeProvider,
+  useConfirmPayment,
+} from '@stripe/stripe-react-native';
+import { STRIPE_PUBLISHABLE_KEY } from '../../constants/App';
+import Button from '../../components/Button/Button';
 
-const Payment = ({navigation}) => {
+const Payment = ({ navigation }) => {
   const donationItemInformation = useSelector(
     state => state.donations.selectedDonationInformation,
   );
+
+  const [isReady, setIsReady] = useState(false);
+  const { confirmPayment, loading } = useConfirmPayment();
+  const user = useSelector(state => state.user);
+
+  const fetchPaymentIntentClientSecret = async () => {
+    const response = await fetch(
+      'http://localhost:3000/create-payment-intent',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          email: user.email,
+          currency: 'usd',
+          amount: donationItemInformation.price * 100,
+        }),
+      },
+    );
+    const { clientSecret } = await response.json();
+    return clientSecret;
+  };
+
+  const handlePayment = async () => {
+    const clientSecret = await fetchPaymentIntentClientSecret();
+    const { error, paymentIntent } = await confirmPayment(clientSecret, {
+      paymentMethodType: 'Card',
+    });
+    if (error) {
+      Alert.alert(
+        'Error has occured with your payment',
+        error.localizedMessage,
+      );
+    } else if (paymentIntent) {
+      Alert.alert('Successful', 'The payment was confirmed successfully!');
+      navigation.goBack();
+    }
+  };
+
   return (
     <SafeAreaView style={[globalStyle.backgroundWhite, globalStyle.flex]}>
       <ScrollView contentContainerStyle={style.paymentContainer}>
@@ -19,14 +63,23 @@ const Payment = ({navigation}) => {
         <Text style={style.donationAmountDescription}>
           You are about to donate {donationItemInformation.price}
         </Text>
-       <View>
+        <View>
           <StripeProvider publishableKey={STRIPE_PUBLISHABLE_KEY}>
-            <CardForm style={style.cardForm} />
+            <CardForm
+              style={style.cardForm}
+              onFormComplete={() => {
+                setIsReady(true);
+              }}
+            />
           </StripeProvider>
         </View>
       </ScrollView>
       <View style={style.button}>
-        <Button title={'Donate'} />
+        <Button
+          title={'Donate'}
+          isDisabled={!isReady || loading}
+          onPress={async () => await handlePayment()}
+        />
       </View>
     </SafeAreaView>
   );
